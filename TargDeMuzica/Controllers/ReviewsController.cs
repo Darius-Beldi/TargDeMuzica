79178b72e0b9a6818a38c9cbf5f1be47536b2be6
EMIL-OVIDIU IVAN <emil-ovidiu.ivan@s.unibuc.ro>
2024-12-15 22:52:51 +0200

    Controller Review + View-uri

--- TargDeMuzica/Controllers/ReviewsController.cs
+++ TargDeMuzica/Controllers/ReviewsController.cs
@@ -1,12 +1,91 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TargDeMuzica.Data;
+using TargDeMuzica.Models;
 
 namespace TargDeMuzica.Controllers
 {
     public class ReviewsController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
+            if (TempData.ContainsKey("message"))
+            {
+                ViewBag.message = TempData["message"].ToString();
+            }
+            var reviews = from review in db.Reviews
+                          orderby review.ReviewContent
+                          select review;
+
+            ViewBag.Reviews = reviews;
             return View();
+        }
+        public ActionResult Show(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            return View(review);
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Review rev)
+        {
+            try
+            {
+                db.Reviews.Add(rev);
+                db.SaveChanges();
+                TempData["message"] = "Review-ul a fost adaugat";
+                return RedirectToAction("Index");
+            }
+
+            catch (Exception e)
+            {
+                return View(rev);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            return View(review);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, Review requestReview)
+        {
+            Review review = db.Reviews.Find(id);
+
+            try
+            {
+
+                review.ReviewContent = requestReview.ReviewContent;
+                db.SaveChanges();
+                TempData["message"] = "Comentariul a fost adugat cu succes!!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                return View(requestReview);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+           Review review = db.Reviews.Find(id);
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+            TempData["message"] = "Review-ul a fost sters";
+            return RedirectToAction("Index");
         }
     }
 }
